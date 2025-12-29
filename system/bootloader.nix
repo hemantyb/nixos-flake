31e42c88bff42116d72570faab38946f976d8657
Hemant Baviskar <hemantyb22@gmail.com>
2025-12-29 20:22:25 +0530

    added: plymouth on boot

--- system/bootloader.nix
+++ system/bootloader.nix
@@ -1,4 +1,12 @@
 {
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    plymouth = {
+      enable = true;
+      theme = "bgrt";
+    };
+  };
 }
