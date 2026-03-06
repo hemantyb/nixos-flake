754e475dc202de5584a8e1939390dc3584c82522
Hemant Baviskar <hemantyb22@gmail.com>
2026-03-06 18:23:36 +0530

    nvf added in nixos helll yeahhh

--- flake.nix
+++ flake.nix
@@ -7,27 +7,27 @@
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nvf.url = "github:notashelf/nvf";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      home-manager,
-    }:
-    let
-      system = "x86_64-linux";
-    in
-    {
-      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          ./configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.users.eman = import ./home.nix;
-          }
-        ];
-      };
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    nvf,
+  }: let
+    system = "x86_64-linux";
+  in {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.users.eman = import ./home.nix;
+        }
+        nvf.nixosModules.default
+      ];
     };
+  };
 }
