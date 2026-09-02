8ab676f25c2c1bbdbe141fb1db4abd58110d3094
Hemant Baviskar <hemantyb22@gmail.com>
2026-09-02 19:00:00 +0530

    organized system service file

--- system/services.nix
+++ system/services.nix
@@ -1,18 +1,19 @@
 {
-  services.displayManager.ly = {
-    enable = true;
-    x11Support = false;
+  services = {
+    displayManager.ly = {
+      enable = true;
+      x11Support = false;
+    };
+    pulseaudio.enable = false;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
+    udisks2.enable = true;
+    openssh.enable = true;
   };
 
-  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
-  services.udisks2.enable = true;
-  services.openssh.enable = true;
 }
